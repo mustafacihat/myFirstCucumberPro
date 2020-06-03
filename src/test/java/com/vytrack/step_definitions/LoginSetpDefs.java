2a2fe631ab87782b6c02345b023f62fda74f558c
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-06-03 09:37:12 +0200

    start to project

---
+++ src/test/java/com/vytrack/step_definitions/LoginSetpDefs.java
@@ -0,0 +1,38 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSetpDefs {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enter the driver information")
+    public void the_user_enter_the_driver_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should be able to login")
+    public void the_user_should_be_able_to_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enter the sales manager information")
+    public void the_user_enter_the_sales_manager_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enter the store manager information")
+    public void the_user_enter_the_store_manager_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
